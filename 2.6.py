90392756f597c972f3ad312cf4f7bf2076775f87
JABKA <76729321+JABKA-228@users.noreply.github.com>
2021-07-14 23:00:35 +0300

    Add files via upload

---
+++ 2.6.py
@@ -0,0 +1,3 @@
+famous_person='Sasuke Uchiha'
+message='It doesnt matter what kind of darkness lies ahead. I will not leave this path! I will gain strength no matter what!'
+print(f'{famous_person}, "{"It doesnt matter what kind of darkness lies ahead. I will not leave this path! I will gain strength no matter what!"}"')
